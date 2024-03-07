9065bca4ae2b1caff5754a7db1138863078ac945
pv18 <pv9998286620@yandex.ru>
2024-03-07 18:15:17 +0300

    fix bug modal width

--- src/components/VideoModal/VideoModal.tsx
+++ src/components/VideoModal/VideoModal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, {FC} from 'react';
 import ModalVideo from 'react-modal-video';
 import s from './VideoModal.module.scss';
 
@@ -10,21 +10,9 @@ interface VideoModalProps {
 
 export const VideoModal: FC<VideoModalProps> = (props) => {
   const { isOpen, setIsOpen, url } = props;
-  const [maxModalSize, setMaxModalSize] = useState(1200);
-
-  // Адаптивный подбор ширины модального окна и видео
-  useEffect(() => {
-    window.addEventListener('resize', resize);
-    return () => window.removeEventListener('resize', resize);
-  });
-
-  const resize = () => {
-    const maxSize = Math.min(1200, window.innerWidth - 40);
-    setMaxModalSize(maxSize);
-  };
 
   return (
-    <div className={s.container} style={{ width: maxModalSize }}>
+    <div className={s.container} style={{ width: '80%' }}>
       <ModalVideo
         isOpen={isOpen}
         url={url}
